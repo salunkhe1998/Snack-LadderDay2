3044c443c5731a14a3776a7329907f0d58b699c5
Ashwini <salunkheashwini1998@gmail.com>
2023-03-16 09:13:05 +0530

    start tje game

---
+++ SnackLadder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnackandLadderDay2
+{
+    public class SnakeLadder
+
+    {
+        public static void StartGame()
+        {
+            int position = 0;
+            int Player1;
+
+            Player1 = position;
+            Console.WriteLine($"Player 1 Position is {Player1}");
+
+        }
+    }
+}
