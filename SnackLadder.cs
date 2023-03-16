c7b155a215d7279c6db106087d925e258db9cb8a
Ashwini <salunkheashwini1998@gmail.com>
2023-03-16 09:21:08 +0530

    update no play ladder

--- SnackLadder.cs
+++ SnackLadder.cs
@@ -11,16 +11,61 @@ namespace SnackandLadderDay2
     {
         public static void StartGame()
         {
-            int position = 0;
-            int Player1;
+            int Player_Position = 0;
+            int Previous_Player_Position = 0;
+            int Next_Player_Position = 0;
+            int Start_Position = 0;
+            int End_Position = 100;
+            int RanNum;
 
-            Player1 = position;
-            Console.WriteLine($"Player 1 Position is {Player1}");
+            void RollDie()
+            {
+                Random random = new Random();
+                RanNum = random.Next(6);
 
-            Random random = new Random();
-            int dice = random.Next(6);
-            Console.WriteLine($"player One rolls die and get position : {dice} ");
+                Choose_Option();
+            }
+
+            void Choose_Option()
+            {
+                int Option_One_NoPlay = 0;
+                int Option_Two_Ladder = 1;
+                int Option_Three_Snake = 2;
+
+                Random ran = new Random();
+                int Choose_Option = ran.Next(3);
+
+                if (Choose_Option == Option_One_NoPlay)
+                {
+                    Player_Position = Previous_Player_Position;
+
+                    RollDie();
+                }
+
+                else if (Choose_Option == Option_Two_Ladder)
+                {
+                    Next_Player_Position = (Previous_Player_Position + RanNum);
+                }
+                else
+                {
+                    Next_Player_Position = (Previous_Player_Position - RanNum);
+                }
+            }
+
+            while (Player_Position >= Start_Position && Player_Position <= End_Position)
+            {
+                RollDie();
+            }
 
         }
+
+
+
     }
+
+
 }
+
+
+
+
